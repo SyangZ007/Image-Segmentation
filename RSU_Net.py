84579a32241740e26122c6cb3b590429a4f9dcb2
SyangZ007 <74520057+SyangZ007@users.noreply.github.com>
2021-12-09 21:55:42 +0800

    Update RSU_Net.py

--- RSU_Net.py
+++ RSU_Net.py
@@ -360,37 +360,22 @@ class RSUNET(Model):
         
         #多输出版，同时监督训练主干输出与多个旁路分支输出
         return main_op,tf.math.sigmoid(hx1_side),tf.math.sigmoid(hx2_side),tf.math.sigmoid(hx3_side),tf.math.sigmoid(hx4_side),tf.math.sigmoid(hx5_side),tf.math.sigmoid(hx6_side)
-    #subclass compile
-    #def compile(self, optimizer, loss_fn):#, metrics):
-       # super(RSUNET, self).compile()
-       # self.opt = optimizer
-        ##self.metrics = metrics
-        #self.loss_fn = loss_fn#loss_fn计算多输出的损失，返回target_loss,total_loss
-    #subclass model.fit train step
-    def train_step(self, data):
-        imgs, gt_masks = data
-        with tf.GradientTape() as tape:
-            #d0, d1, d2, d3, d4, d5, d6 = self(imgs, training=True) # Forward pass
-            y_pred = self(imgs, training=True)
-            # Compute our own loss
-            #tar_loss,total_loss = self.loss_fn(d0, d1, d2, d3, d4, d5, d6, gt_masks)#接受主干输出+6个旁路输出，返回target_loss,total_loss
-            tar_loss,total_loss = self.compiled_loss(gt_masks,y_pred)
-        # 使用total loss计算gradients
-        trainable_vars = self.trainable_variables
-        grads = tape.gradient(total_loss, trainable_vars)
-        # Update weights
-        self.optimizer.apply_gradients(zip(grads, trainable_vars))
-        # Compute our own metrics
-        #self.metrics.update_state(y, d0)
-        #return {"mean iou": self.metrics.result()}
-    #@property
-    #def metrics(self):
-        # We list our `Metric` objects here so that `reset_states()` can be
-        # called automatically at the start of each epoch
-        # or at the start of `evaluate()`.
-        # If you don't implement this property, you have to call
-        # `reset_states()` yourself at the time of your choosing.
-        #return [self.metrics]
+
+#subclass model.fit train step
+def train_step(self, data):
+    imgs, gt_masks = data
+    with tf.GradientTape() as tape:
+        #d0, d1, d2, d3, d4, d5, d6 = self(imgs, training=True) # Forward pass
+        y_pred = self(imgs, training=True)
+        # Compute our own loss
+        #tar_loss,total_loss = self.loss_fn(d0, d1, d2, d3, d4, d5, d6, gt_masks)#接受主干输出+6个旁路输出，返回target_loss,total_loss
+        tar_loss,total_loss = self.compiled_loss(gt_masks,y_pred)
+    # 使用total loss计算gradients
+    trainable_vars = self.trainable_variables
+    grads = tape.gradient(total_loss, trainable_vars)
+    # Update weights
+    self.optimizer.apply_gradients(zip(grads, trainable_vars))
+
 ##############################Auto-Encoder###########################
 #Auto-Encoder上采样块
 class Auto_DecodeBlock(Model):
