a11c495d3cf646d03787194a0e68ecd680db7261
SyangZ007 <74520057+SyangZ007@users.noreply.github.com>
2022-02-19 12:18:19 +0800

    Update RSU_Net.py

--- RSU_Net.py
+++ RSU_Net.py
@@ -396,7 +396,85 @@ class RSUNET(Model):
             return main_op,tf.math.sigmoid(hx1_side),tf.math.sigmoid(hx2_side),tf.math.sigmoid(hx3_side),tf.math.sigmoid(hx4_side),tf.math.sigmoid(hx5_side),tf.math.sigmoid(hx6_side)
         else:
             return main_op#仅监督训练主干输出版本
+############RSU Net without Attention Block############
+class NoAttetionRSUNET(Model):
+    '''不使用注意力机制的RSU Net'''
+    def __init__(self,in_ch=3,out_ch=4,is_muti_output=False):
+        super(NoAttetionRSUNET,self).__init__()
+        self.is_muti_output=is_muti_output#是否采用多输出版本模型
+        self.stage1 = RSU7(in_ch,32,64)#size:b*h*w*64
+        self.pool12 = layers.MaxPool2D(pool_size=2,strides=2,padding='same')#ceil_mode=True
+        self.stage2 = RSU6(64,32,128)
+        self.pool23 = layers.MaxPool2D(pool_size=2,strides=2,padding='same')#ceil_mode=True
+        self.stage3 = RSU5(128,64,256)
+        self.pool34 = layers.MaxPool2D(pool_size=2,strides=2,padding='same')#ceil_mode=True
+        self.stage4 = RSU4(256,64,256)
+        self.pool45 = layers.MaxPool2D(pool_size=2,strides=2,padding='same')#ceil_mode=True
+        self.stage5 = RSU4F(256,128,256)
+        self.pool56 = layers.MaxPool2D(pool_size=2,strides=2,padding='same')#ceil_mode=True
+        self.stage6 = RSU4F(256,128,512)
+        # decoder 五个上采样块 transposed_conv + conv_block   
+        self.stage5d = DecodeBlock(128)
+        self.stage4d = DecodeBlock(64)
+        self.stage3d = DecodeBlock(64)
+        self.stage2d = DecodeBlock(64)
+        self.stage1d = DecodeBlock(64)
+        # decoder 6个旁路输出分支,旁路输出模式：3*3卷积输出四通道特征图(线性激活)==>上采样到target size==>所有旁路输出拼接、融合==>1*1卷积、四通道sigmoid激活输出
+        self.side6 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.side5 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.side4 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.side3 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.side2 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.side1 = layers.Conv2D(4,3,1,padding='same')#,activation='relu')
+        self.outconv = layers.Conv2D(out_ch,kernel_size=1,activation='sigmoid')#输出层1*1卷积调整通道数，padding='valid'
+    def call(self,x):
+        #stage 1
+        hx1 = self.stage1(x)#size:b*128*800*c
+        hx = self.pool12(hx1)#size:b*64*400*c
+        #stage 2
+        hx2 = self.stage2(hx)#size:b*64*400*c
+        hx = self.pool23(hx2)#size:b*32*200*c
+        #stage 3
+        hx3 = self.stage3(hx)#size:b*32*200*c
+        hx = self.pool34(hx3)#size:b*16*100*c
+        #stage 4
+        hx4 = self.stage4(hx)#size:b*16*100*c
+        hx = self.pool45(hx4)#size:b*8*50*c
+        #stage 5
+        hx5 = self.stage5(hx)#size:b*8*50*c
+        hx = self.pool56(hx5)#size:b*4*25*c
+        #stage 6 bottle neck
+        hx6 = self.stage6(hx)#size:b*4*25*c  相对于原始尺寸32倍下采样
+        #decoder解码器部分
+        hx6_side = self.side6(hx6)#解码网络各尺度6旁路输出
+        hx6_side = _upsample_like(hx6_side,hx1,mode='normal')
         
+        hx5d = self.stage5d(hx6,hx5)#解码stage1：b*8*50*c
+        hx5_side = self.side5(hx5d)#解码网络尺度5旁路输出
+        hx5_side = _upsample_like(hx5_side,hx1,mode='normal')
+        
+        hx4d = self.stage4d(hx5d,hx4)#解码stage2：b*16*100*c
+        hx4_side = self.side4(hx4d)#解码网络尺度4旁路输出
+        hx4_side = _upsample_like(hx4_side,hx1,mode='normal')
+
+        hx3d = self.stage3d(hx4d,hx3)#解码stage3：b*32*200*c
+        hx3_side = self.side3(hx3d)#解码网络尺度3旁路输出
+        hx3_side = _upsample_like(hx3_side,hx1,mode='normal')
+
+        hx2d = self.stage2d(hx3d,hx2)#解码stage4：b*64*400*c
+        hx2_side = self.side2(hx2d)#解码网络尺度2旁路输出
+        hx2_side = _upsample_like(hx2_side,hx1,mode='normal')
+
+        hx1d = self.stage1d(hx2d,hx1)#解码stage5：b*128*800*c
+        #解码网络尺度1无需旁路输出
+        hx1_side = self.side1(hx1d)
+        
+        main_op=self.outconv(tf.concat([hx1_side,hx2_side,hx3_side,hx4_side,hx5_side,hx6_side],axis=-1))#主干sigmoid激活输出
+        if self.is_muti_output:
+            #多输出版，同时监督训练主干输出与多个旁路分支输出
+            return main_op,tf.math.sigmoid(hx1_side),tf.math.sigmoid(hx2_side),tf.math.sigmoid(hx3_side),tf.math.sigmoid(hx4_side),tf.math.sigmoid(hx5_side),tf.math.sigmoid(hx6_side)
+        else:
+            return main_op#仅监督训练主干输出版本
 #subclass model.fit train step,现在改用在notebook导入RSUNET后，重新写一个子类模型
 def train_step(self, data):
     imgs, gt_masks = data
